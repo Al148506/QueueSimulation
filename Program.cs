24850b711c8a4db2d2935f7f59a62e75d29b78e4
Al148506 <125323112+Al148506@users.noreply.github.com>
2024-10-13 17:22:53 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class Program
+    {
+        private Stack<int> pila1 = new Stack<int>();
+        private Stack<int> pila2 = new Stack<int>();
+        static void Main(string[] args)
+        {
+           /* Program program = new Program();
+            program.Insert(10);
+            program.Insert(20);
+            Console.WriteLine($"The queue is empty {program.IsEmpty()}"); 
+            Console.WriteLine($"The first number is {program.Seek()}"); 
+
+            Console.ReadLine();*/
+            
+        }
+        public void Insert(int x)
+        {
+            pila1.Push(x);
+        }
+        public int Delete()
+        {
+
+            if (pila1.Count == 1)
+            {
+                return pila1.Pop();
+            }
+            else
+            {
+                Transfer();
+                int result = pila2.Pop();
+                Restore();
+                return result;
+            }
+        }
+        public int Seek()
+        {
+            if(pila1.Count == 0)
+            {
+                throw new ArgumentException("The queue is empty");
+            }
+            if (pila1.Count == 1)
+            {
+                return pila1.Peek();
+            }
+            else
+            {
+                Transfer();
+                int result = pila2.Peek();
+                Restore();
+                return result;
+
+            }
+        }
+
+        public void Restore()
+        {
+            while (pila2.Count > 0)
+            {
+                pila1.Push(pila2.Pop());
+
+            }
+        }
+
+        public void Transfer()
+        {
+            while (pila1.Count > 0)
+            {
+                pila2.Push(pila1.Pop());
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return pila1.Count == 0;
+        }
+
+      /*  public static string Something()
+        {
+            return "algo";
+        }
+
+        public static bool Login(string user, string pass)=>
+            user == "Alex" && pass == "1234" ? true : false;  
+        */
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,81 @@
+using Queue;
+
+namespace QueueTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestPeekOnce()
+        {
+            Program program = new Program();
+            program.Insert(1);
+            program.Insert(2);
+            int number = program.Seek();
+            Assert.AreEqual(1, number);
+        }
+
+        [TestMethod]
+        public void TestPeekEmpty()
+        {
+            Program program = new Program();
+
+            Assert.ThrowsException<ArgumentException>(() => program.Seek());
+        }
+
+        [TestMethod]
+        public void TestPeekMultiple()
+        {
+            Program program = new Program();
+            program.Insert(1);
+            program.Insert(2);
+            int number1 = program.Seek();
+            program.Insert(3);
+            int number2 = program.Seek();
+            Assert.AreEqual(1, number1);
+            Assert.AreEqual(number1, number2);
+        }
+
+        [TestMethod]
+        public void TestUnqueueUnique()
+        {
+            Program program = new Program();
+            program.Insert(1);
+            Assert.AreEqual(1, program.Delete());
+            Assert.IsTrue(program.IsEmpty());
+        }
+
+        [TestMethod]
+        public void TestUnqueueMultiple()
+        {
+            Program program = new Program();
+            program.Insert(1);
+            program.Insert(2);
+            Assert.AreEqual(1, program.Delete());
+            Assert.AreEqual(2, program.Delete());
+            Assert.IsTrue(program.IsEmpty());
+        }
+
+
+
+
+
+
+
+
+        /* [TestMethod]
+         public void TestSomething()
+         {
+             string result = Program.Something();
+             Assert.AreEqual("algo",result);
+         }
+         [TestMethod]
+         public void TestLogin()
+         {
+             bool result = Program.Login("Alex","1234");
+             Assert.IsTrue(result);
+         }
+        */
+    }
+}
